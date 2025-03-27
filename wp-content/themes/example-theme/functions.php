65f1759243b0ec6bda0b30ae93d3c5a4c43ae3c9
Kaapo <142903354+Kaapeli-seta@users.noreply.github.com>
2025-03-27 11:36:37 +0200

    assingment 5

--- wp-content/themes/example-theme/functions.php
+++ wp-content/themes/example-theme/functions.php
@@ -5,13 +5,7 @@ register_nav_menu( 'main-menu', __( 'Main Menu' ) );
 
 add_action( 'after_setup_theme', 'register_my_menu' );
 
-function add_h1_to_title( $title ): string {
-    return '<h1>' . $title . '</h1>';
-}
-
-add_filter( 'the_title', 'add_h1_to_title' );
-
-function theme_setup() {
+function theme_setup(): void {
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'custom-header' );
